691e27638c20404d5e139bdb738dc845405b85e4
Quty <quty.jain@gmail.com>
2018-05-09 16:38:04 -0400

    Correcting test class

--- src/test/java/com/thoughtmechanix/confsvr/ConfsvrApplicationTests.java
+++ src/test/java/com/thoughtmechanix/confsvr/ConfsvrApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.books.confsvr.ConfsvrApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ConfsvrApplication.class)
 public class ConfsvrApplicationTests {
 
 	@Test
